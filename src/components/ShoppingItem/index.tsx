48cc0f6c343dee564e4bee84b145209f58cfe53d
Raimundo Neto <rasneto147@gmail.com>
2023-05-06 19:52:43 -0300

    add isChedcked in databse ready

--- src/components/ShoppingItem/index.tsx
+++ src/components/ShoppingItem/index.tsx
@@ -3,10 +3,24 @@ import React, { useEffect, useState } from 'react'
 import styles from './styles';
 import { AntDesign } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
+import  {doc, updateDoc, db} from '../../config/Firebase/index'
+
 
 const ShoppingItem = (props: any) => {
   const { item } = props
   const [isChecked,setIsChecked] = useState(item.isChecked);
+
+  const updateIsCheked = async () => {
+    const shoppingRef = doc(db, "shopping", item.id);
+
+    await updateDoc(shoppingRef, {
+        isChecked: isChecked
+    });
+  }
+
+  useEffect(() => {
+    updateIsCheked();
+  }, [isChecked])
 
   return (
     <View style={styles.container}>
